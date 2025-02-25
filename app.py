7be7bdb429a98333d38c4d4f33103aea26fbd555
Adrian Aro <aroadrianchristopher@gmail.com>
2025-02-25 23:31:44 +0800

    try to connect database and create table by running this app

--- app.py
+++ app.py
@@ -1,9 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from templates.model import db_connection
+import psycopg2
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'  # Return a string       
+#para matawag pala yung function na db_connect sa model.py
+
+db_connection()
+
+#Database connection
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="admin"  )
+    return conn
+
+
+get_db_connection()
+
+#SELECT all data from the database
+
+
+@app.route('/', methods = ['GET','POST'])
+def create():
+     if request.method == 'GET':
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.close()
+        conn.close()
+        return render_template('create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run the app in debug modes
